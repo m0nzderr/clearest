293cc3677bfae5b46bdb0d83b3f8318f0380cd0e
Illya Kokshenev <sou@illya.com.br>
2015-10-23 23:53:38 -0200

    tiny browser-side impl of  Builder interface

---
+++ browser/builder.js
@@ -0,0 +1,39 @@
+/*
+ * Clearest Framework
+ * Provided under MIT License.
+ * Copyright (c) 2012-2015  Illya Kokshenev <sou@illya.com.br>
+ */
+
+var html = require("../runtime/html"),
+    commons = require("../commons"),
+    Builder = require("../runtime/builder");
+
+/**
+ * Simple builder that wraps around browser implementation.
+ *
+ * THIS IS A BROSWER-ONLY CODE
+ *
+ * @param document
+ * @constructor
+ */
+commons.inherit(DomBuilder, Builder)
+
+/* istanbul ignore next */
+function DomBuilder(document) {
+
+    this.render = function (view, presentation) {
+        // this is it
+        view.innerHTML = html(presentation);
+    };
+
+    this.getView = getElementById;
+
+    this.getId = function (view) {
+        return view.getAttribute("id");
+    }
+}
+
+
+module.exports = DomBuilder;
+
+
